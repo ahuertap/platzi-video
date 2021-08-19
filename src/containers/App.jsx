50d7299f80eae8b945ddb1d1a08fbcfc4cec7fb4
Angel Huerta <angeladrianhp@hotmail.com>
2021-08-19 13:36:46 -0500

    Conectamos nuestro proyecto a una fake api y alimentamos a nuestro carrusel

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -9,40 +9,56 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/movies');
+      const data = await response.json();
+      return setVideos(data);
+    } catch (error) {
+      return console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Search />
 
-      <Categories title="Mi Lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            {videos.mylist.map((item) => {
+              return <CarouselItem key={item.id} title={item.title} year={item.year} contentRating={item.contentRating} duration={item.duration} cover={item.cover} />;
+            })}
+          </Carousel>
+        </Categories>
+      )}
 
-      <Categories title="Tendencias">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.trends.length > 0 && (
+        <Categories title="Tendencias">
+          <Carousel>
+            {videos.trends.map((item) => {
+              return <CarouselItem key={item.id} title={item.title} year={item.year} contentRating={item.contentRating} duration={item.duration} cover={item.cover} />;
+            })}
+          </Carousel>
+        </Categories>
+      )}
 
-      <Categories title="Originales">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.originals.length > 0 && (
+        <Categories title="Originales">
+          <Carousel>
+            {videos.originals.map((item) => {
+              return <CarouselItem key={item.id} title={item.title} year={item.year} contentRating={item.contentRating} duration={item.duration} cover={item.cover} />;
+            })}
+          </Carousel>
+        </Categories>
+      )}
 
       <Footer />
 
