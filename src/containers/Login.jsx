faa53034326b45f66ce86c8b033f600237700cec
Angel Huerta <angeladrianhp@hotmail.com>
2021-09-08 18:54:20 -0500

    Add Register component w/styles and route

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
         </section>
         <p className="login__container--register">
           No tienes ninguna cuenta
-          <a href="/">Regístrate</a>
+          <a href="/register">Regístrate</a>
         </p>
       </section>
     </section>
