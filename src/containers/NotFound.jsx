6f93ad726e377b96e9044866dc5ed4e060ebdbcc
Angel Huerta <angeladrianhp@hotmail.com>
2021-09-09 10:22:10 -0500

    Add a 404 not found component

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>No encontrado</h1>
+      <h2><a href="/">Regresa al Home</a></h2>
+    </>
+  );
+};
+
+export default NotFound;
