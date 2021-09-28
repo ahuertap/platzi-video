e904e4406c9106f3171a2ae9aa6f73be8db34115
Angel Huerta <angeladrianhp@hotmail.com>
2021-09-28 17:53:42 -0500

    Agregamos un player para nuestros videos

---
+++ src/containers/Player.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/destructuring-assignment */
+import React, { useLayoutEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { getVideoSource } from '../actions';
+import NotFound from './NotFound';
+
+import '../assets/styles/components/Player.scss';
+
+const Player = (props) => {
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useLayoutEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
+    <div className="player">
+      <video controls autoPlay>
+        <source src={props.playing.source} type="video/mp4" />
+      </video>
+      <div className="player-back">
+        <button type="button" onClick={() => props.history.goBack()}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  ) : <NotFound />;
+};
+
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
